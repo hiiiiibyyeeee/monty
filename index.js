6fa753feaa7c5debb9a0d804232194309fd4aec0
Monty404 <83779017+Moonty404@users.noreply.github.com>
2021-09-20 19:46:12 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const db = require('quick.db');
 const { TOKEN, PREFIX, AVATARURL, BOTNAME, } = require(`./config.json`);
 const figlet = require("figlet");
 const client = new Client({ disableMentions: `` , partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
-client.login("ODAyOTk1MzUzMjA5MDEyMjM0.YA3Vsw.Srgstmmb37EzdFBhhiLFHBj3wU8");
+client.login(".YA3Vsw.Srgstmmb37EzdFBhhiLFHBj3wU8");
 client.commands = new Collection();
 client.setMaxListeners(0);
 client.prefix = PREFIX;
